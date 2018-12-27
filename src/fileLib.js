2936a35a312c78d25ba54777fac6e229e77ff106
Arif <44020819+azimarif@users.noreply.github.com>
2018-12-27 14:50:06 +0530

    Refactor wc, extract getFileDetails in fileLib.js
    getFileCountDetail now gets the filename & its content, no need to
    read the file data here

--- src/fileLib.js
+++ src/fileLib.js
@@ -1,17 +1,19 @@
 const FILE_READ_ENCODING = 'utf-8';
 
 const readFile = function (file, fs) {
-  //if (isFileExists(file, fs)) {
-    return fs.readFileSync(file, FILE_READ_ENCODING);
-  //}
+  return fs.readFileSync(file, FILE_READ_ENCODING);
 };
-
-// const isFileExists = function (file, fs) {
-//   return fs.existsSync(file);
-// };
 
 const isSingleFile = function (files) {
   return files.length == 1;
 };
 
-module.exports = { readFile, isSingleFile };
+const getFileDetails = function (file, fs) {
+  let fileDetail = {
+    content: readFile(file, fs),
+    name: file
+  }
+  return fileDetail;
+}; 
+
+module.exports = { isSingleFile, getFileDetails };
--- src/lib.js
+++ src/lib.js
@@ -6,31 +6,31 @@ const {
 
 const { parseInput } = require('../src/inputParser.js');
 const { singleFileFormatter, multipleFileFormatter } = require('../src/formatter.js');
-const { readFile, isSingleFile } = require('../src/fileLib.js');
+const { isSingleFile, getFileDetails } = require('../src/fileLib.js');
 
-const wc = function (userArgs, fs) {
+const wc = function(userArgs, fs) {
   let { fileNames, options } = parseInput(userArgs);
-  let fileDetails = [];
-  fileNames.forEach(fileName => {
-    let fileCountDetail = getFileCountDetail(fileName, options, fs);
-    fileDetails.push(fileCountDetail);
-  });
+  let fileDetails = fileNames.map(fileName => getFileDetails(fileName, fs));
+
+  let fileCountDetails = fileDetails.map(fileDetail =>
+    getFileCountDetail(fileDetail, options)
+  );
+
   if (!isSingleFile(fileNames)) {
-    return multipleFileFormatter(fileDetails);
+    return multipleFileFormatter(fileCountDetails);
   }
-  let singleFileDetail = fileDetails[0];
-  return singleFileFormatter(singleFileDetail);
+  let firstFileCountDetail = fileCountDetails[0];
+  return singleFileFormatter(firstFileCountDetail);
 };
 
-const getFileCountDetail = function (fileName, options, fs) {
-  const fileContent = readFile(fileName, fs);
-  let fileDetail = { count: [], fileName };
+const getFileCountDetail = function(fileDetail, options) {
+  let fileCountDetail = { count: [], fileName: fileDetail.name };
   options.forEach(option => {
     let operation = selectCount(option);
-    let count = operation(fileContent);
-    fileDetail.count.push(count);
+    let count = operation(fileDetail.content);
+    fileCountDetail.count.push(count);
   });
-  return fileDetail;
+  return fileCountDetail;
 };
 
 const selectCount = function (option) {
